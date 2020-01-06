604db9ebb12c9640d94c59777d08e6a0c06f5b8a
Dusan Stanic <dusan@drobilica.com>
2020-01-06 02:29:17 +0100

    update

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 import feedparser
-
+from bs4 import BeautifulSoup
 # @app.route('/')
 # @app.route('/index')
 # def index():
@@ -25,32 +25,57 @@ import feedparser
 def return_cache(tmp_file,update_time_sec):
     return None
 
+# @app.route('/')
+# def index():
+#     entries_sorted=None
+#     if entries_sorted!=None:
+#         return render_template(
+#         'index.html.j2',
+#         entries=entries_sorted
+#         )
+#     RSS_URLS = [
+#         'http://www.gameinformer.com/b/MainFeed.aspx?Tags=preview',
+#         'http://www.polygon.com/rss/group/news/index.xml',
+#         ]
+#
+#     entries = []
+#     for url in RSS_URLS:
+#         entries.extend(feedparser.parse(url).entries)
+#
+#     entries_sorted = sorted(
+#         entries,
+#         key=lambda e: e.published_parsed,
+#         reverse=True)
+#     f = open("output.txt", "w+")
+#     for i in entries_sorted:
+#         f.write(str(i))
+#     f.close()
+#     return render_template(
+#         'index.html.j2',
+#         entries=entries_sorted
+#         )
+
 @app.route('/')
+@app.route('/index')
 def index():
-    entries_sorted=None
-    if entries_sorted!=None:
-        return render_template(
-        'index.html.j2',
-        entries=entries_sorted
-        )
-    RSS_URLS = [
-        'http://www.gameinformer.com/b/MainFeed.aspx?Tags=preview',
-        'http://www.polygon.com/rss/group/news/index.xml',
-        ]
 
-    entries = []
-    for url in RSS_URLS:
-        entries.extend(feedparser.parse(url).entries)
-
-    entries_sorted = sorted(
-        entries,
-        key=lambda e: e.published_parsed,
-        reverse=True)
-    f = open("output.txt", "w+")
-    for i in entries_sorted:
-        f.write(str(i))
-    f.close()
+    NewsFeed = feedparser.parse("https://www.eurogamer.net/?format=rss&type=article")
+    entry = NewsFeed.entries[0]
+    entrynumber = len(NewsFeed.entries)
+    print(f"Number of RSS posts {len(NewsFeed.entries)}")
+    entries_sorted = entry.keys()
+    soup = BeautifulSoup(entry.summary)
+    eimage = soup.find('img')
+    print()
     return render_template(
         'index.html.j2',
-        entries=entries_sorted
+        entries=entries_sorted,
+        esummary = entry.summary,
+        etitle = entry.title ,
+        edate = entry.published ,
+        etest = entry.summary ,
+        eimage = eimage,
+        elink = entry.link,
+        entiresnumber = entrynumber
+
         )
